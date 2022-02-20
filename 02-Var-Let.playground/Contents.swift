4083c249650aed502ca667c22bea4bfd7fe43d74
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-02-20 00:58:32 -0600

    var and let

---
+++ 02-Var-Let.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let maximumNumberOfLoginAttempts=3;
+
+var currentLoginAttempt=0;
+
+print("The constant's value is: ", maximumNumberOfLoginAttempts)
+print("The variable's value is: ", currentLoginAttempt)
